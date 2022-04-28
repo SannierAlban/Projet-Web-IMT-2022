f96505ff97222ad60a0f3bd9da99642b2a7349f5
AntoineDevConnexy <dev@connexy.link>
2022-04-28 14:18:29 +0200

    Change on login & others

--- front-end/js/login.js
+++ front-end/js/login.js
@@ -2,30 +2,6 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 };
 
-function checkRegister(){
-    pseudo = document.getElementById("pseudo-login");
-    mdp = document.getElementById("mdp-login");
-    console.log(pseudo.value+mdp.value);
-    //Link to RegisterUser();
-}
-
-function RegisterUser(){
-    var xhr = new XMLHttpRequest(),
-    method = "GET";
-    var url = "/user/register/userName="+pseudo.value+"&userPassword="+mdp.value;  
-    xhr.open(method, url, true);
-    xhr.setRequestHeader("Accept", "application/json; charset=UTF-8");
-    xhr.onreadystatechange = function () {
-        if(xhr.readyState === 4 && xhr.status === 200) {
-            showMessage(2);
-        } else {
-            showMessage(3);
-        }
-    };
-    xhr.send();
-}
-
-
 function checkUser(){
     pseudo = document.getElementById("pseudo-login");
     mdp = document.getElementById("mdp-login");
@@ -37,7 +13,7 @@ function checkUser(){
     }
 }
 
-function checkUserExist(){
+function checkUser1(){
     var xhr = new XMLHttpRequest(),
     method = "GET";
     var url = "user_name="+pseudo.value+"&user_password="+mdp.value;  
@@ -54,19 +30,14 @@ function checkUserExist(){
 async function showMessage(type){
     let messageToReturn;
     if (type == 0) {
-        
-        document.getElementById("message-login-box").innerHTML = "Login mot de passe incorrect";
+        var title = "Login mot de passe incorrect";
+        document.getElementById("message-login-box").innerHTML = title;
     } else if (type == 1) {
+        var title = "Connexion réussi ...";
         sessionStorage.setItem('userID','4');
-        document.getElementById("message-login-box").innerHTML = "Connexion réussi ...";
+        document.getElementById("message-login-box").innerHTML = title;
         await sleep(1000);
         window.location.href = "menu/index.html"; 
-    } else if (type == 2) {
-        document.getElementById("message-login-box").innerHTML = "Enregistrement réussi";
-        await sleep(3000);
-        document.getElementById("message-login-box").innerHTML = "";
-    } else if (type == 3) {
-        document.getElementById("message-login-box").innerHTML = "Enregistrement fail";
     }
 
 }
